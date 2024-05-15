acabec14b69f4444823d663a61bb6db3da945d58
ariel.duarte <arielpy@gmail.com>
2024-05-14 21:47:00 -0300

    Added mechanismo to build/render MDX files with Astro and Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: github - dark,
+          transformers: [
+            transformerCopyButton({
+              visibility: "hover",
+              feedbackDuration: 2_500,
+            }),
+          ],
+        },
+      ],
+    ],
+  },
+  integrations: [tailwind({ applyBaseStyles: false }), mdx()],
 });
---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { z, defineCollection } from "astro:content";
+
+const postsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    image: z.string().url(),
+    pubDate: z.string(),
+    author: z.string(),
+    profile: z.string(),
+    type: z.string(),
+    brief: z.string(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = {
+  post: postsCollection,
+};
